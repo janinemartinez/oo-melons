d25649673123020cc046b8c0b110d42b42a42811
Your Name <you@example.com>
2020-01-17 15:51:00 -0800

    rush hour

--- melons.py
+++ melons.py
@@ -1,7 +1,16 @@
 """Classes for melon orders."""
+import random
+import calendar
+import datetime
+
+
+x = calendar.weekday
+
 
 class AbstractMelonOrder():
     """An abstract base class that other Melon Orders inherit from."""
+
+    # date_order = datetime.datetime.now()
 
     def __init__(self, species, qty):
 
@@ -9,13 +18,26 @@ class AbstractMelonOrder():
         self.qty = qty
         self.shipped = False
 
+    def get_base_price(self):
+        base_price = random.randrange(5, 9)
+
+        current_day = calendar.weekday(self.date_order.year, self.date_order.month, self.date_order.day)
+        current_time = int(str(self.date_order.time())[0:2])
+
+        if current_day >= 0 and current_day < 5 and current_time > 8 and current_time <= 11:
+            base_price += 4
+
+        # add 4
+        # current__time = whatever the function gives us
+        return base_price
+
     def get_total(self):
-        """Calculate price, including tax.""" 
+        """Calculate price, including tax."""
 
         if self.species == "Christmas":
-            base_price = 7.50
+            base_price = self.get_base_price() * 1.5
         else:
-            base_price = 5
+            base_price = self.get_base_price()
         self.total = (1 + self.tax) * self.qty * base_price
 
         if self.order_type == "international":
@@ -33,7 +55,7 @@ class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
     tax = 0.08
     order_type = "domestic"
-    
+    date_order = datetime.datetime.now() 
 
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
@@ -63,6 +85,7 @@ class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
     tax = 0.17
     order_type = "international"
+    date_order = datetime.datetime.now() 
 
     def __init__(self, species, qty, country_code):
         super().__init__(species, qty)
@@ -95,13 +118,15 @@ class InternationalMelonOrder(AbstractMelonOrder):
 
 class GovernmentMelonOrder(AbstractMelonOrder):
     """Government Melon Class"""
-    tax = 0
 
-    def __init__(self, species, qty, country_code):
-        super().__init__(species, qty) 
+    tax = 0
+    order_type = "domestic"
+    date_order = datetime.datetime.now() 
+
+    def __init__(self, species, qty):
+        super().__init__(species, qty)
 
         self.passed_inspection = False
 
-    def mark_inspection(passed):
+    def mark_inspection(self, passed):
         self.passed_inspection = passed
-
